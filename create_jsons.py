a9219b37114f6add06737ff01449857de1e786aa
Hallee Shearer <halleeshearer@outlook.com>
2024-08-07 19:19:23 -0400

    fix create_jsons.py to allow for multiple types of scripts

--- create_jsons.py
+++ create_jsons.py
@@ -12,14 +12,15 @@ if not os.path.exists('vis'):
     os.makedirs('vis')
 
 # loop through all scripts in the scripts directory
-# for filename in os.listdir('scripts'):
-#     if filename.endswith('.py'): # TODO: change to allow .R files too
-#         # open the script file
-#         with open('scripts/' + filename, 'r') as f:
-#             script = f.read()
-#         # create a json object that contains input as an empty list and output as an empty list
-#         json_obj = {'input': [], 'output': []}
-#         # write the json object to a file
-#         with open('vis/' + filename[:-4] + '.json', 'w') as f:
-#             json.dump(json_obj, f)
+for filename in os.listdir('scripts'):
+    # if filename is a script name
+    if filename.endswith('.py') or filename.endswith('.R') or filename.endswith('.sh') or filename.endswith('.mat'):
+        # open the script file
+        with open('scripts/' + filename, 'r') as f:
+            script = f.read()
+        # create a json object that contains input as an empty list and output as an empty list
+        json_obj = {'input': [], 'output': []}
+        # write the json object to a file
+        with open('vis/' + filename[:-4] + '.json', 'w') as f:
+            json.dump(json_obj, f)
 
