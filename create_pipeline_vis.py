33715675bc7280243c7e280f58f9436cd9a5c266
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-08-07 14:44:20 -0400

    Create create_pipeline_vis.py

---
+++ create_pipeline_vis.py
@@ -0,0 +1,3 @@
+# a function that loops through all scripts in the scripts directory, and for each one creates a matching .json file in the vis directory
+
+# if vis directory does not exist, create it
