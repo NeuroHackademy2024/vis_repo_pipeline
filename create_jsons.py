04fc881aba7bfc0181a348c3d1abb7e90a201fa9
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-08-07 18:48:48 -0400

    create script to create json files based on scripts

---
+++ create_jsons.py
@@ -0,0 +1,25 @@
+# a function that loops through all scripts in the scripts directory, and for each one creates a matching .json file in the vis directory
+
+# save the current path as a variable
+import os
+import json
+
+# change the current directory to the directory of this script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# if vis directory does not exist in this github repo, create it
+if not os.path.exists('vis'):
+    os.makedirs('vis')
+
+# loop through all scripts in the scripts directory
+# for filename in os.listdir('scripts'):
+#     if filename.endswith('.py'): # TODO: change to allow .R files too
+#         # open the script file
+#         with open('scripts/' + filename, 'r') as f:
+#             script = f.read()
+#         # create a json object that contains input as an empty list and output as an empty list
+#         json_obj = {'input': [], 'output': []}
+#         # write the json object to a file
+#         with open('vis/' + filename[:-4] + '.json', 'w') as f:
+#             json.dump(json_obj, f)
+
--- create_pipeline_vis.py
+++
@@ -1,3 +0,0 @@
-# a function that loops through all scripts in the scripts directory, and for each one creates a matching .json file in the vis directory
-
-# if vis directory does not exist, create it
